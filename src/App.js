1ffbfaee0103e682f5fd54b2f3a7738eae8f7b0d
Elliott Jones <elliottohj44@gmail.com>
2019-06-18 14:27:16 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Palette from "./components/Palette";
+import seedColors from "./seedColors";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Palette />
+    </div>
+  );
+}
+
+export default App;
