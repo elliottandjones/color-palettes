66c26ed22fb6abda649b05961e2b29b2e6af04ab
Elliott Jones <elliottohj44@gmail.com>
2019-06-19 22:10:34 -0400

    add ColorBox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Palette from "./components/Palette";
+import Palette from "./components/Palette/Palette";
 import seedColors from "./seedColors";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Palette />
+      <Palette {...seedColors[4]} />
     </div>
   );
 }
--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import ColorBox from "../ColorBox/ColorBox";
 
 class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map(color => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
       <div className="palette">
         {/* Navbar */}
-        <div className="palette-colors"> {/* bunch of color boxes */}</div>
+        <div className="palette-colors">{colorBoxes}</div>
         {/* footer */}
       </div>
     );
