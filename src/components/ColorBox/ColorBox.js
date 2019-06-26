7e102c13b61b2d48aa7dd726e60fc6652ab4da07
Elliott Jones <elliottohj44@gmail.com>
2019-06-26 15:40:43 -0400

    add copy to clipboard function

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
 class ColorBox extends Component {
@@ -11,12 +12,14 @@ class ColorBox extends Component {
     const { name, background } = this.props;
     return (
       <div style={{ background }} className="color-box">
-        <div className="copy-container">
-          <div className="box-content">
-            <span>{name}</span>
+        <CopyToClipboard text={background}>
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
           </div>
-          <button className="copy-button">Copy</button>
-        </div>
+        </CopyToClipboard>
         <span className="see-more">More</span>
       </div>
     );
